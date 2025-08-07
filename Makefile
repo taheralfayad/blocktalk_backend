3e8afa1e42b06a13d4b4f6058a4164963fe44070
taheralfayad <taheralfayad@gmail.com>
2025-08-06 21:29:44 -0400

    add version migration scripts

--- Makefile
+++ Makefile
@@ -7,3 +7,15 @@ migrate-up:
 
 migrate-all-down:
 	docker compose exec $(CONTAINER_NAME) go run migrate/migrate.go allDown
+
+migrate-to-version:
+ifeq ($(VERSION),)
+	$(error USAGE: make migrate-to-version VERSION=<version_number>)
+endif
+	docker compose exec $(CONTAINER_NAME) go run migrate/migrate.go toVersion $(VERSION)
+
+force-migrate:
+ifeq ($(VERSION),)
+	$(error USAGE: make force-migrate VERSION=<version_number>)
+endif
+	docker compose exec $(CONTAINER_NAME) go run migrate/migrate.go force $(VERSION)
--- migrate/migrate.go
+++ migrate/migrate.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
@@ -46,7 +47,6 @@ func migrateAllDown() error {
 	}
 	defer m.Close()
 
-	// Example: Roll back all migrations
 	if err := m.Down(); err != nil && err != migrate.ErrNoChange {
 		log.Fatalf("migration down failed: %v", err)
 	}
@@ -56,10 +56,84 @@ func migrateAllDown() error {
 	return nil
 }
 
+func migrateToVersion(version int) error {
+	m, err := migrate.New(
+		"file://sql",
+		os.Getenv("DATABASE_URL"),
+	)
+	if err != nil {
+		log.Fatalf("failed to create migrate instance: %v", err)
+	}
+	defer m.Close()
+
+	if err := m.Migrate(uint(version)); err != nil && err != migrate.ErrNoChange {
+		log.Fatalf("migration to version %d failed: %v", version, err)
+	}
+
+	log.Printf("Migration to version %d complete.", version)
+
+	return nil
+}
+
+func forceVersion(version int) error {
+	m, err := migrate.New(
+		"file://sql",
+		os.Getenv("DATABASE_URL"),
+	)
+	if err != nil {
+		log.Fatalf("failed to create migrate instance: %v", err)
+	}
+	defer m.Close()
+
+	if err := m.Force(version); err != nil {
+		log.Fatalf("force migration to version %d failed: %v", version, err)
+	}
+
+	log.Printf("Force migration to version %d complete.", version)
+
+	return nil
+}
+
 func main() {
 	funcs := map[string]func() error{
 		"up":      runMigrations,
 		"allDown": migrateAllDown,
+		"toVersion": func() error {
+			if len(os.Args) < 3 {
+				return log.Output(2, "Please provide a version number to migrate to.")
+			}
+			version := os.Args[2]
+
+			intVersion, err := strconv.Atoi(version)
+
+			if err != nil {
+				return log.Output(2, "Invalid version number provided.")
+			}
+
+			if intVersion < 0 {
+				return log.Output(2, "Version number must be a non-negative integer.")
+			}
+
+			return migrateToVersion(intVersion)
+		},
+		"force": func() error {
+			if len(os.Args) < 3 {
+				return log.Output(2, "Please provide a version number to force.")
+			}
+			version := os.Args[2]
+
+			intVersion, err := strconv.Atoi(version)
+
+			if err != nil {
+				return log.Output(2, "Invalid version number provided.")
+			}
+
+			if intVersion < 0 {
+				return log.Output(2, "Version number must be a non-negative integer.")
+			}
+
+			return forceVersion(intVersion)
+		},
 	}
 
 	if len(os.Args) < 2 {
